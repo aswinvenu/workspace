187c9e7637d01d8585b02ba956284d383918089c
Aswin Venu <aswin@aswin>
2015-11-26 02:36:23 +0530

    modified:   readbluetooth.py

--- readbluetooth.py
+++ readbluetooth.py
@@ -1,7 +1,15 @@
 import serial
 
+#change the port as per your setup. In this case my device is connected to
+#/dev/rfcomm0. It will be the defualt one
+
 reader = serial.Serial("/dev/rfcomm0",9600)
 
 while(True):
     data = reader.readline().strip("/n/r")
     print data
+    #need to write:
+    #calculate the distance travelled 
+    #plots
+    #breath
+
